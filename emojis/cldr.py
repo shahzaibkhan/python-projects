a01d7cb143805726ea031278665c0eff0c7805d8
Shahzaib Khan <shahzaib198@gmail.com>
2022-07-01 12:28:47 +0500

    [ADD] Print Emojis using Python

---
+++ emojis/cldr.py
@@ -0,0 +1,8 @@
+# grinning face
+print("\N{grinning face}""-Grinning Face")
+  
+# slightly smiling face
+print("\N{slightly smiling face}""-Slightly Smiling Face")
+  
+# winking face
+print("\N{winking face}""-Winking Face")
---
+++ emojis/unicode.py
@@ -0,0 +1,8 @@
+# grinning face
+print("\U0001f600" "-Grinning Face")
+  
+# Hugging Face
+print("\U0001F917""-Hugging Face")
+  
+# rolling on the floor laughing
+print("\U0001F923""-Rolling on the floor laughing")
